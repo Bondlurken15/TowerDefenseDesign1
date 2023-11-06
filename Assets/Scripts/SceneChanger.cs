68e4a331073aa603442e72a6970e79075d016011
bonasieric <eric.berntsson@elev.ga.lbs.se>
2023-11-06 11:32:33 +0100

    Jumpscare and scenes

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] Canvas jumpscareCanvas;
+    [SerializeField] GameObject videoPlayer;
+
+    private void Start()
+    {
+        jumpscareCanvas.gameObject.SetActive(false);
+        videoPlayer.gameObject.SetActive(false);
+    }
+
+    public void ChangeScene(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        StartCoroutine(JumpscareRoutine());
+    }
+
+    IEnumerator JumpscareRoutine()
+    {
+        jumpscareCanvas.gameObject.SetActive(true);
+        videoPlayer.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(1);
+
+        Application.Quit();
+    }
+}
